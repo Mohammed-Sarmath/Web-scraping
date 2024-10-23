ef8dd0f1a3c139216d1f3e5704a4cb309df65298
Mohammed-Sarmath <gentleman.samad@gmail.com>
2024-10-23 18:03:45 +1100

    Updated comments in Ecommerce-scraper.py

--- Ecommerce-scraper.py
+++ Ecommerce-scraper.py
@@ -2,22 +2,22 @@ from bs4 import BeautifulSoup
 import urllib.request
 import requests
 
-# Step 1: Requesting the web page
+# Request the web page
 page_url = 'https://blacktownmarkets.com.au/au/products-second-hand-items-for-sale'
 response = requests.get(page_url)
 
-# Step 2: checking if the request is successful
+# Check if it's sucessfull(200 means successfull) 
 if response.status_code == 200:
     print("Website Permission Granted")
 
-    #Step 3: Parse the page content
+    # Parse it
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    #Step 4 Find all product names and prices
+    # All the product names and prices were in the 'h5' tag element with each class name, below code is appropriate
     Names = soup.find_all('h5', class_="fw-600 mb-1")
     Prices = soup.find_all('h5', class_="fw-600 text-theme")
 
-    #Step 5: Print names and prices if lengths match
+    # If the length matches, below for loop prints all names and prices together at once with the help of 'zip'
     if len(Names) == len(Prices):
         for name, price in zip(Names, Prices):
             print(name.text.strip(), price.text.strip())  # .strip() removes extra spaces
